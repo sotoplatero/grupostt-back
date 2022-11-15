6cb77dedc970c98e6717bf305fee0b0acd61cb9f
Damián Soto Platero <soto.platero@gmail.com>
2022-11-15 18:53:45 -0300

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+# BACK Prueba Técnica GrupoSTT
--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,8 @@
 import { Deta } from 'deta';
 
 // Initialize with a Project Key
-const deta = Deta(process.env.DETA_PROJECT_KEY); 
+const deta = Deta('e00bffh9_cqwGSHHqn4K8FPsFgeZBNC7a3JWGrUzr'); 
+// const deta = Deta(process.env.DETA_PROJECT_KEY); 
 
 export const db = deta.Base('comments'); 
 
